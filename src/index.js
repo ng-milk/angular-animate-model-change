24faccc6bfc349a2cca9c4bef1a2f78a6e645b95
Dan Mindru <mindrudan@gmail.com>
2015-12-18 16:49:08 +0200

    Check if the directive element has transition-duration set and use it as a default

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@
  * License: MIT
  */
 
-angular.module('dm.animateModelChange', [])
-       .directive('animateModelChange', animateModelChangeDirective);
+ angular.module('dm.animateModelChange', [])
+ .directive('animateModelChange', animateModelChangeDirective);
 
 
 /*
@@ -14,16 +14,16 @@ angular.module('dm.animateModelChange', [])
  * @param <String> attrs.incrementClass: the class to apply when incrementing.
  * @param <String> attrs.decrementClass: the class to apply when decrementing.
 * @param <String> attrs.nonNumberClass: the class to apply when the model is not a number.
- */
+*/
 animateModelChangeDirective.$inject = ['$timeout'];
 function animateModelChangeDirective($timeout){
   function animateModelChangeLink(scope, element, attrs){
     var timer = null,
-        timeout = attrs.timeout || 300,
-        currentClass = parseClassName(element.attr('class')) || 'model',
-        incrementClass = attrs.incrementClass || currentClass + '--increment',
-        decrementClass = attrs.decrementClass || currentClass + '--decrement',
-        nonNumberClass = attrs.nonNumberClass || currentClass + '--non-number';
+    timeout = (attrs.timeout || getTransitionDuration(getComputedStyle(element[0]), attrs.$normalize)) || 300,
+    currentClass = parseClassName(element.attr('class')) || 'model',
+    incrementClass = attrs.incrementClass || currentClass + '--increment',
+    decrementClass = attrs.decrementClass || currentClass + '--decrement',
+    nonNumberClass = attrs.nonNumberClass || currentClass + '--non-number';
 
     function parseClassName(className){
       var classComps = className.split(' ').filter(function(item){
@@ -37,6 +37,7 @@ function animateModelChangeDirective($timeout){
 
     function modelChanged(newVal, oldVal){
       if(newVal !== oldVal){
+        // The non-number class will be added by default.
         var changeClass = nonNumberClass;
 
         // Clear previous timeout.
@@ -79,4 +80,45 @@ function animateModelChangeDirective($timeout){
     restrict: 'A',
     link: animateModelChangeLink
   };
+}
+
+
+/*
+ * @param <Object> computedStyle: an object containing all element computed styles.
+ * @param <Function> normalize: a method that converts a given string to a camelCase format.
+ *
+ * @return <Int>: transition duration in milliseconds.
+ */
+function getTransitionDuration(computedStyle, normalize){
+  var prefixes = ' webkit moz ms o khtml'.split( ' ' ),
+      result = 0,
+      duration,
+      delay,
+      prefix;
+
+  for(var i = 0; i < prefixes.length; i++){
+    prefix = prefixes[i] + '-';
+
+    if(prefixes[i] === ''){
+      prefix = '';
+    }
+
+    duration = computedStyle[normalize(prefix + 'transition-duration')];
+
+    if(duration){
+      duration = (duration.indexOf('ms') >- 1) ? parseFloat(duration) : parseFloat(duration) * 1000;
+
+      // Check if there's a delay.
+      delay = computedStyle[normalize(prefix + 'transition-delay' )];
+
+      if(delay){
+        duration += (delay.indexOf('ms') >- 1) ? parseFloat(delay) : parseFloat(delay) * 1000;
+      }
+
+      result = duration;
+      break;
+    }
+  }
+
+  return result;
 }
